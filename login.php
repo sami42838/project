e6228842f130fb56e73c772c62602a7a9a98ba18
sami42838 <152060102+sami42838@users.noreply.github.com>
2025-03-29 22:37:12 +0530

    Update login.php

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows == 1) {
         $_SESSION['username'] = $user;
-        header("Location: dashboard.php");
+        header("Location: project.html");
         exit();
     } else {
         echo "<script>
